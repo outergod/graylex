359c2c9f967796043df6689000c4d607efa07f05
Alexander Kahl <e-user@fsfe.org>
2010-07-20 10:52:36 +0200

    added testing stubs

--- test/buffered-input-stream.lisp
+++ test/buffered-input-stream.lisp
@@ -1,4 +1,4 @@
-;;;; graylex - graylex-test.asd
+;;;; graylex - buffered-input-stream.lisp
 ;;;; Copyright (C) 2010  Alexander Kahl <e-user@fsfe.org>
 ;;;; This file is part of graylex.
 ;;;; graylex is free software; you can redistribute it and/or modify
@@ -14,22 +14,8 @@
 ;;;; You should have received a copy of the GNU General Public License
 ;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(in-package :cl-user)
+(in-package :graylex-test)
 
-(defpackage :graylex-test-system
-  (:use :cl :asdf))
-
-(in-package :graylex-test-system)
-
-(asdf:defsystem :graylex-test
-                :description "graylex test package"
-                :version "0.0.1"
-                :author "Alexander Kahl <e-user@fsfe.org>"
-                :license "GPLv3+"
-                :depends-on (:graylex :stefil)
-                :components
-                ((:module "test"
-                          :components
-                          ((:file "package")
-                           (:file "buffered-input-stream" :depends-on ("package"))
-                           (:file "lexer-input-stream"    :depends-on ("package"))))))
+(in-suite all)
+(defsuite buffer)
+(in-suite buffer)
